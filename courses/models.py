a16153796d36614274a42fc7c44cf7a4cfdd9dc6
chenrun-Mac <17610780919@163.com>
2019-06-16 10:27:23 +0800

    create courses app, create models

---
+++ courses/models.py
@@ -0,0 +1,52 @@
+import datetime
+
+from django.db import models
+
+
+# Create your models here.
+class Course(models.Model):
+    name = models.CharField(max_length=50, verbose_name="课程名")
+    desc = models.CharField(max_length=200, verbose_name="课程描述")
+    detail = models.TextField(verbose_name="课程详细")
+    degree = models.CharField(choices=(("primary", "初级"), ("middle", "中级"), ("high", "高级")), verbose_name="课程难度")
+    learn_times = models.IntegerField(default=0, verbose_name="学习时长（分钟）")
+    students = models.IntegerField(default=0, verbose_name="学习人数")
+    fav_nums = models.IntegerField(default=0, verbose_name="收藏人数")
+    image = models.ImageField(upload_to="courses/%Y/%m", verbose_name="封面图", max_length=100)
+    click_num = models.IntegerField(default=0, verbose_name="点击数")
+    add_time = models.DateTimeField(default=datetime.datetime.now, verbose_name="添加时间")
+
+    class Meta:
+        verbose_name = "课程"
+        verbose_name_plural = verbose_name
+
+
+class Lesson(models.Model):
+    course = models.ForeignKey(Course, verbose_name="课程", on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, verbose_name="章节名")
+    add_time = models.DateTimeField(default=datetime.datetime.now, verbose_name="添加时间")
+
+    class Meta:
+        verbose_name = "章节"
+        verbose_name_plural = verbose_name
+
+
+class Video(models.Model):
+    lesson = models.ForeignKey(Lesson, verbose_name="章节", on_delete=models.CASCADE)
+    name = models.CharField(max_length=100, verbose_name="视频名称")
+    add_time = models.DateTimeField(default=datetime.datetime.now, verbose_name="添加时间")
+
+    class Meta:
+        verbose_name = "视频"
+        verbose_name_plural = verbose_name
+
+
+class CourseResource(models.Model):
+    name = models.CharField(max_length=100, verbose_name="名称")
+    course = models.ForeignKey(Course, verbose_name="课程", on_delete=models.CASCADE)
+    download = models.FileField(upload_to="courses/resourse/%Y/%m", verbose_name="资源文件", max_length=100)
+    add_time = models.DateTimeField(datetime.datetime.now, verbose_name="添加时间")
+
+    class Meta:
+        verbose_name = "课程资源"
+        verbose_name_plural = verbose_name
